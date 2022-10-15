458e8e5e279bd897ae760c1a0022db842bbea6e0
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-10-15 15:20:55 +0200

    Function ReadingCurrenciesFromJSONFile added

--- ConCurrency/DataBase.cpp
+++ ConCurrency/DataBase.cpp
@@ -1,6 +1,19 @@
 #include "DataBase.hpp"
 
+DataBase::DataBase()
+{
+    _mapOfCurrencies = getCurrenciesFromJSONFile();
+}
+
 std::map<std::string, double> DataBase::getMapOfCurrencies()
 {
-    return mapOfCurrencies;;
+    return _mapOfCurrencies;;
+}
+
+std::map<std::string, double> DataBase::getCurrenciesFromJSONFile() const
+{
+    std::ifstream ifs("currencies.json");
+    json j = json::parse(ifs);
+    std::map<std::string, double> m = j;
+    return m;
 }
