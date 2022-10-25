9a4db5e28ff23cb31f36ebe93b59df21970d50ca
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-10-25 20:12:04 +0200

    Pause and displayTitle functions

--- ConCurrency/ConCurrency.cpp
+++ ConCurrency/ConCurrency.cpp
@@ -2,23 +2,6 @@
 
 int main()
 {
-	/*void displayTitle()
-	{
-
-	}*/
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << "\033[97m       $$$$                            $$$$                                                                  \033[0m" << std::endl;
-	std::cout << "\033[97m      $    $                          $    $                                                                 \033[0m" << std::endl;
-	std::cout << "\033[97m     $           $$$      $$$$$      $          $      $  $   $  $   $   $$$$   $$$$$     $$$$    $    $     \033[0m" << std::endl;
-	std::cout << "     $         $     $    $     $    $          $      $  $ $    $ $    $    $  $    $   $    $   $    $     " << std::endl;
-	std::cout << "     $        $       $   $     $    $          $      $  $      $      $$$$$   $    $   $        $    $     " << std::endl;
-	std::cout << "     $         $     $    $     $    $          $      $  $      $      $       $    $   $    $    $$$$$     " << std::endl;
-	std::cout << "\033[90m      $    $     $$$      $     $     $    $      $$$$    $      $       $$$$   $    $    $$$$         $     \033[0m" << std::endl;
-	std::cout << "\033[90m       $$$$                            $$$$                                                          $$$     \033[0m" << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-
 	try
 	{
 		MainManager m;
